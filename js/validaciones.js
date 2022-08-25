91f26550b0c90360017fbb48b1c33403e7cb8f51
Iara Gimenez-Antoniow <iaragimeneza@gmail.com>
2022-08-25 11:30:44 -0300

    validacion de fecha y mayor de edad

---
+++ js/validaciones.js
@@ -0,0 +1,23 @@
+const inputNacimiento = document.querySelector("#birth");
+
+inputNacimiento.addEventListener("blur", (evento) => {
+    validarNacimiento(evento.target);
+});
+
+function validarNacimiento(input){
+    const fechaCliente = new Date(input.value);
+    let mensaje = "";
+    if(!mayorEdad(fechaCliente)){
+        mensaje = "Debes tener al menos 18 Años";
+    }
+    input.setCustomValidity(mensaje);
+}
+
+function mayorEdad(fecha){
+    const fechaActual = new Date();
+    const diferenciaFechas = new Date(
+        fecha.getUTCFullYear() + 18, 
+        fecha.getUTCMonth(), 
+        fecha.getUTCDate());
+    return diferenciaFechas <= fechaActual;
+}
